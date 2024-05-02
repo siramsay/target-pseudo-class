08f578836e7a69ed75afa1c75e63d1db3c6cb41e
simon <simon@designkojo.net>
2024-05-03 06:47:36 +0900

    initial commit

---
+++ action.js
@@ -0,0 +1,8 @@
+var preventDefault = document.getElementById('prevent-target');
+
+preventDefault.addEventListener('click', function (e) {
+  //return false;
+  //e.preventDefault();
+});
+
+// https://stackoverflow.com/questions/1601933/how-do-i-stop-a-web-page-from-scrolling-to-the-top-when-a-link-is-clicked-that-t
